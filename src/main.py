b0bd211aa826ae175d2f813c0e0fcad32be5bab0
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2026-05-15 16:03:14 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+
+def main():
+    node = TextNode("It worked!", TextType.BOLD)
+    print(node)
+
+
+if __name__ == "__main__":
+    main()
