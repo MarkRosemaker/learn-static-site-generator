b5a3504d484796d7338c09698409ea06404967ee
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2026-05-17 12:10:00 +0200

    add tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,33 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_diff_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is node has a different text", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_diff_type(self):
+        node = TextNode("Hi!", TextType.BOLD)
+        node2 = TextNode("Hi!", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_no_type_is_plain(self):
+        node = TextNode("Just plain text", TextType.PLAIN)
+        node2 = TextNode("Just plain text")
+        self.assertEqual(node, node2)
+
+    def test_no_type_is_plain(self):
+        node = TextNode("Just plain text", TextType.PLAIN)
+        node2 = TextNode("Just plain text")
+        self.assertEqual(node, node2)
+
+    def test_url(self):
+        link = TextNode("Check out this link", TextType.LINK, "example.com")
+        link2 = TextNode("Check out this link", TextType.LINK, "example.com")
+        linkMissing = TextNode("Check out this link", TextType.LINK)
+        self.assertEqual(link, link2)
+        self.assertNotEqual(link, linkMissing)
+
 
 if __name__ == "__main__":
     unittest.main()
