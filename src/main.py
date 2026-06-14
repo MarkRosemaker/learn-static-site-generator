21271098a30935faa89334ce062a883379d7c36f
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2026-06-14 13:51:18 +0200

    create copy_to_destination

--- src/main.py
+++ src/main.py
@@ -1,7 +1,32 @@
 from textnode import TextNode, TextType
+import os, shutil
+
+DIR_STATIC = "static"
+DIR_DST = "public"
+
+
+def copy_to_destination():
+    if os.path.exists(DIR_DST):
+        shutil.rmtree(DIR_DST)
+
+    copy_to_destination_recursive(DIR_STATIC, DIR_DST)
+
+
+def copy_to_destination_recursive(src: str, dst: str):
+    if os.path.isfile(src):
+        print(f"Copying {dst}...")
+        shutil.copy(src, dst)
+        return
+
+    print(f"Creating {dst}/...")
+    os.mkdir(dst)
+
+    for f in os.listdir(src):
+        copy_to_destination_recursive(os.path.join(src, f), os.path.join(dst, f))
 
 
 def main():
+    copy_to_destination()
     node = TextNode("It worked!", TextType.BOLD)
     print(node)
 
